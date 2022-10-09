fd6aa52968d1a2463edaad7004979dd4e4a0bd15
Sarah B <petcapitol@gmail.com>
2022-10-09 13:40:05 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+document.addEventListener('DOMContentLoaded',documentReady , false);
+
+function documentReady(){
+
+    var navmenuid = document.getElementById("navmenuid");
+    navmenuid.style.display = "none";
+document.getElementById("menuid").onclick=function(){
+
+
+    if (navmenuid.style.display === "none") {
+        navmenuid.style.display = "block";
+    } else {
+        navmenuid.style.display = "none";
+    }
+};
+}
