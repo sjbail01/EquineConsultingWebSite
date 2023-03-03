f13b7d468d846454d81537047c49e43623560783
Sarah B <petcapitol@gmail.com>
2023-03-03 14:12:09 -0500

    [wb2 some js]

--- main.js
+++ main.js
@@ -1,19 +1,41 @@
+document.addEventListener('DOMContentLoaded', documentReady, false);
 
+function documentReady() {
 
-document.addEventListener('DOMContentLoaded',documentReady , false);
+    /* var navmenuid = document.getElementById("navmenuid");
+     navmenuid.style.display = "none";
+     document.getElementById("menuid").onclick = function () {
+         /* var myArray = [{
+                  "numberofHorses": "1-3 horses",
+                  "price": "$60.00"
+              },
+              {
+                  "numberofHorses": "4-6 horses",
+                  "price": "$90.00"
+              },
+              {
+                  "numberofHorses": "7-10 horses",
+                  "price": "$120.00"
+              },
+          ]
+          buildTable(myArray);
 
-function documentReady(){
+          function buildTable(dataTraining) {
+              var tbprice = document.getElementById("tbprice");
 
-    var navmenuid = document.getElementById("navmenuid");
-    navmenuid.style.display = "none";
-document.getElementById("menuid").onclick=function(){
+              for (var i = 0; i < dataTraining.lenght; i++) {
+                  var row = `<tr>
+                              <td>${dataTraining[i].numberOfHorses}</td>
+                              <td>${dataTraining[i].price}</td>
+                              </tr>`
+                  tbprice.inn erHTML + row
+              }
+          }
 
-
-    if (navmenuid.style.display === "none") {
-        navmenuid.style.display = "block";
-    } else {
-        navmenuid.style.display = "none";
-    }
-};
+          if (navmenuid.style.display === "none") {
+              navmenuid.style.display = "block";
+          } else {
+            navmenuid.style.display = "none";
+          }
+     };*/
 }
-
